a98b75902c236100e06e04e3c2244c3ee5f771de
Максим <maxum4uk@tut.by>
2020-02-19 22:08:02 +0300

    123

--- src/main/java/com/max/customproject/controllers/RegistrationController.java
+++ src/main/java/com/max/customproject/controllers/RegistrationController.java
@@ -12,10 +12,11 @@ public class RegistrationController {
     public ModelAndView registrationPage(@RequestParam String login1, String email, String psw, String psw1) {
         ModelAndView model = new ModelAndView();
 
-        //System.out.println(login1);
-        //System.out.println(email);
-        //System.out.println(psw);
-        //System.out.println(psw1);
+        boolean rezultLogin = false;
+        boolean rezultQuantityLettersLogin = RegistrationController.checkQuantityLettersLogin(login1);    //проверяет что логин должен состоять из 5-15 символов
+        boolean rezultLettersLogin = RegistrationController.checkLettersLogin(login1);   //проверяет что логин должен состоять только из букв и цифр и символа @ и . (точка)
+        System.out.println(rezultQuantityLettersLogin);
+        System.out.println(rezultLettersLogin);
 
         model.addObject("login", login1);
         model.addObject("mail", email);
@@ -24,6 +25,44 @@ public class RegistrationController {
 
         model.setViewName("index");
         return model;
+    }
+
+    public static boolean checkQuantityLettersLogin(String login) {
+        boolean bol = false;
+        if (login.length() < 16 && login.length() > 4) {
+            bol = true;
+        }
+        return bol;
+    }
+
+    public static boolean checkLettersLogin(String login) {
+        boolean bol = false;
+        char[] result = login.toCharArray();
+        boolean bol1 = false;
+        boolean bol2 = false;
+        boolean bol3 = false;
+        boolean bol4 = false;
+
+        for (int i = 0; i < login.length(); i++) {
+            int a = (int) result[i];
+            if ((a > 64 && a < 91) || (a > 96 && a < 123)) {
+                bol1 = true;
+            }
+            if (a > 47 && a < 58) {
+                bol2 = true;
+            }
+            if (a == 64) {
+                bol3 = true;
+            }
+            if (a == 46) {
+                bol4 = true;
+            }
+        }
+
+        if (bol1 == true && bol2 == true && bol3 == true && bol4 == true) {
+            bol = true;
+        }
+        return bol;
     }
 }
 
